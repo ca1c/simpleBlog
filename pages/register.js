44fe12b06a38284991e77b25793b03b9b89cc500
ca1c <gegs921@gmail.com>
2022-03-28 18:06:11 -0700

    built structure of register page

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+import styles from '../styles/login.module.css'
+import axios from 'axios';
+
+import Head from 'next/head'
+
 export default function Register() {
     return (
         <>
-            <h1>Register</h1>
+            <Head>
+                <title>SimpleBlog - Register</title>
+            </Head>
+            <div className={styles.container}>
+                <h1 className={styles.subheading}>Register</h1>
+                <label>Username:</label>
+                <input type="text" placeholder="username" className={styles.input} />
+                <label>Email:</label>
+                <input type="text" placeholder="example@gmail.com" className={styles.input} />
+                <label>Password:</label>
+                <input type="text" placeholder="password" className={styles.input} />
+                <button className={styles.button}>Register</button>
+            </div>
         </>
     )
 }
