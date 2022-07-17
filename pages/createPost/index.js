3aef16f0a1b7cd456a36eb6dd055f607fddafe50
ca1c <gegs921@gmail.com>
2022-07-17 11:38:57 -0700

    blog posts can be saved in database

--- pages/createPost/index.js
+++ pages/createPost/index.js
@@ -2,27 +2,19 @@ import styles from '../../styles/createPost.module.css';
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 import Navigation from '../../components/navigation.component';
 import AuthenticateUser from '../../util/authenticate';
 
-export default function createPost() {
+export default function CreatePost() {
+    const cookies = new Cookies();
     const [authenticated, setAuthenticated] = useState(false);
     const [bodyText, setBodyText] = useState("");
     const [titleText, setTitleText] = useState("");
     const [subheadingText, setSubheadingText] = useState("");
     const router = useRouter();
-
-    useEffect(() => {
-        AuthenticateUser.then((username) => {
-            setAuthenticated(true);
-            console.log(username);
-        })
-        .catch((err) => {
-            console.log(err);
-            router.push('/login');
-        })
-    }, [])
 
     function handleBodyChange(e) {
         let value = e.target.value;
@@ -39,6 +31,32 @@ export default function createPost() {
         setSubheadingText(value);
     }
 
+    function submitPost() {
+        let sessId = cookies.get("sessID");
+
+        const blogPost = {
+            sessId: sessId,
+            title: titleText,
+            subheading: subheadingText,
+            bodyText: bodyText, 
+        }
+
+        axios.post("http://localhost:8080/api/createPost", blogPost).then((res) => {
+            console.log(res.data.success, res.data.message);
+        })
+    }
+
+    useEffect(() => {
+        AuthenticateUser.then((username) => {
+            setAuthenticated(true);
+            console.log(username);
+        })
+        .catch((err) => {
+            console.log(err);
+            router.push('/login');
+        })
+    }, []);
+
     return (
         <>
             <Navigation authenticated={authenticated}/>
@@ -53,6 +71,9 @@ export default function createPost() {
                     </div>
                     <div className="mb-3">
                         <textarea className="form-control" placeholder="body" value={bodyText} onChange={handleBodyChange} rows="3"></textarea>
+                    </div>
+                    <div className="mb-3">
+                        <button className="btn btn-primary" onClick={submitPost}>Submit</button>
                     </div>
                     <div className="title">
                         <h4>{titleText}</h4>
